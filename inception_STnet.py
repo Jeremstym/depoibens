091cb554653fffd4dd0363a6df1c0c76d675a017
Jeremstym <jeremstym@gmail.com>
2023-06-07 15:21:27 +0100

    Add embedding for all images

--- inception_STnet.py
+++ inception_STnet.py
@@ -1,3 +1,239 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import pickle as pkl
+import os
+from glob import glob
+from tqdm import tqdm
+
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import PIL
+from PIL import Image
+import cv2
+import matplotlib.pyplot as plt
+from torchvision import transforms
+
+
+### ------------ Data processing ---------------------
+
+
+# def tsv_processing(df: pd.DataFrame, gene_list: list) -> pd.DataFrame:
+#     mask = list(df.filter(regex="ambiguous"))
+#     filtered_df = df[df.columns.drop(mask)]
+#     filtered_df.rename(columns={"Unnamed: 0": "id"}, inplace=True)
+#     filtered_df.set_index("id", inplace=True)
+
+#     return filtered_df[gene_list]
+
+# with open(r"E:\ST-Net\data\hist2tscript\BRCA\std_genes_avg.pkl", "rb") as f:
+#     df_std = pkl.load(f)
+# bestgene = list(df_std.index[:900])
+
+# path = r"E:\ST-Net\data\hist2tscript\BRCA\BC23270"
+# with open(path + "\BC23270_E2.tsv", "rb") as f:
+#     gene_270E2 = pd.read_csv(f, sep="\t")
+
+# gene_270E2 = tsv_processing(gene_270E2, bestgene)
+# torch.tensor(gene_270E2.values).size()
+
+### ------------ Network ---------------
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+model = torch.hub.load("pytorch/vision:v0.10.0", "inception_v3", pretrained=True)
+model.eval()
+model.to(device)
+
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
+        
+    def forward(self, x):
+        return x
+    
+model.fc = Identity()
+
+### -------------- Image embedding ---------------
+
+def image_embedding(path, model=model, device=device):
+    cell = Image.open(path)
+    preprocess = transforms.Compose(
+        [
+            transforms.Resize(299),
+            transforms.CenterCrop(299),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+            ),
+        ]
+    )
+    input_tensor = preprocess(cell)
+    input_batch = input_tensor.unsqueeze(0)  # create a mini-batch of 1 sample
+    input_batch = input_batch.to(device)
+    with torch.no_grad():
+        output = model(input_batch)
+    return output
+
+def embed_all_images(path):
+    
+    embeddings = []
+    for sub_path in tqdm(glob(path + '/*/', recursive=True)):
+        for path_image in tqdm(glob(sub_path + '/*.jpg', recursive=True)):
+            embeddings.append(image_embedding(path_image))
+
+    return embeddings
+
+if __name__ == "__main__":
+    path = "/import/pr_minos/jeremie/data"
+    embeddings = embed_all_images(path)
+    with open("embeddings.pkl", "wb") as f:
+        pkl.dump(embeddings, f)
+
+
+### --------------- Brouillon ---------------
+
+# preprocess = transforms.Compose(
+#     [
+#         transforms.Grayscale(num_output_channels=1),
+#         transforms.Resize(30),
+#         transforms.CenterCrop(30),
+#         transforms.ToTensor(),
+#         transforms.Normalize(mean=[0.485], std=[0.229]),
+#         transforms.Lambda(lambda x: torch.flatten(x)),
+#     ]
+# )
+
+# preprocess(cell1).size()
+
+path = r"E:\ST-Net\data\hist2tscript\BRCA\BC23270"
+cell1 = Image.open(path + "\BC23270_E2.jpg")
+cell1 = cv2.imread(path + "\BC23270_E2.jpg", cv2.COLOR_BGR2RGB)
+# cell1 = cell1.resize((300,300))
+cell1.shape
+tensor = torch.tensor(np.transpose(cell1, (2, 0, 1)))
+tensor.size()
+# cv2.imshow("hello", cv2.resize(cell1, (300,300)))
+# plt.show()
+
+
+tensor = torch.randn((1, 28, 28)).unsqueeze(0)
+tensor = nn.Conv2d(1, 32, 3, 1)(tensor)
+tensor = nn.Conv2d(32, 64, 3, 1)(tensor)
+tensor = nn.Dropout2d(0.25)(tensor)
+tensor = F.max_pool2d(tensor, 2)
+tensor.size()
+nn.ConvTranspose2d()
+
+torch.flatten(tensor, 1).size()
+
+tensor = torch.randn((1, 28, 28)).unsqueeze(0)
+transforms.CenterCrop(28)(tensor)
+
+from torchvision.datasets import MNIST
+import torch.utils.data as data
+
+
+batch_size = 128
+transform = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+)
+train_set = MNIST(
+    r"C:\Jérémie\Stage\IBENS\depo\dataplus",
+    train=True,
+    transform=transform,
+    download=True,
+)
+train_loader = data.DataLoader(
+    train_set, batch_size=batch_size, shuffle=True, num_workers=2, drop_last=True
+)
+
+
+with open(path + "\BC23270_D2_complete.pkl", "rb") as f:
+    df_complete = pkl.load(f)
+
+
+class PandasDataset(data.Dataset):
+    def __init__(self, dataframe):
+        self.dataframe = dataframe
+
+    def __len__(self):
+        return len(self.dataframe)
+
+    def __getitem__(self, index):
+        if torch.is_tensor(index):
+            index = index.tolist()
+        coords = list(self.dataframe.iloc[index][:-1])
+        label = self.dataframe.iloc[index][-1]
+        return [coords, label]
+
+
+df_complete["label"] = df_complete["tumor"].apply(lambda x: 1 if x == "tumor" else 0)
+df_complete.drop(columns=["title", "lab", "tumor"], inplace=True)
+
+df = PandasDataset(df_complete)
+
+df[1]
+# df_complete = PandasDataset(df_complete)
+
+train_loader = data.DataLoader(df, batch_size=4)
+next(iter(train_loader))[1]
+
+for X, y in train_loader:
+    print(X)
+    print(y)
+    break
+
+# df_complete.to_numpy()
+
+# from collections import OrderedDict
+
+# from ignite.engine import Engine
+# # from ignite.handlers import *
+# # from ignite.metrics import *
+# # from ignite.utils import *
+# # from ignite.contrib.metrics.regression import *
+# # from ignite.contrib.metrics import
+
+# # create default evaluator for doctests
+
+# def eval_step(engine, batch):
+#     return batch
+
+# default_evaluator = Engine(eval_step)
+
+# # create default optimizer for doctests
+
+# param_tensor = torch.zeros([1], requires_grad=True)
+# default_optimizer = torch.optim.SGD([param_tensor], lr=0.1)
+
+# # create default trainer for doctests
+# # as handlers could be attached to the trainer,
+# # each test must define his own trainer using `.. testsetup:`
+
+# def get_default_trainer():
+
+#     def train_step(engine, batch):
+#         return batch
+
+#     return Engine(train_step)
+
+# # create default model for doctests
+
+# default_model = nn.Sequential(OrderedDict([
+#     ('base', nn.Linear(4, 2)),
+#     ('fc', nn.Linear(2, 1))
+# ]))
+
+
+# from ignite.metrics import FID
+
+# metric = FID(num_features=1, feature_extractor=default_model)
+# metric.attach(default_evaluator, "fid")
+# y_true = torch.ones(10, 4)
+# y_pred = torch.ones(10, 4)
+# state = default_evaluator.run([[y_pred, y_true]])
+# print(state.metrics["fid"])
