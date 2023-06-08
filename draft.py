d4a019a484c84a29eb0da59cee6cca97e9cfdcb2
Jeremstym <jeremstym@gmail.com>
2023-06-08 15:02:55 +0100

    Check cuda availability

--- draft.py
+++ draft.py
@@ -8,12 +8,13 @@ import os
 import torch
 
 if __name__ == '__main__':
-    if torch.cuda.is_available():
-        available_gpus = [torch.cuda.device(i) for i in range(torch.cuda.device_count())]
-        print(available_gpus)
-    else:
-        print("No GPU available")
-
-
-def f(x:str) -> str:
-    return x + "a"
+    torch.cuda.device_count()
+    torch.cuda.get_device_name(0)
+    torch.cuda.is_available()
+    torch.cuda.current_device()
+    torch.cuda.device(0)
+    torch.cuda.empty_cache()
+    torch.cuda.memory_allocated()
+    torch.cuda.memory_cached()
+    torch.cuda.max_memory_allocated()
+    torch.cuda.max_memory_cached()
