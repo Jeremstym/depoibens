e7909c38a65467f412c48760b71c1f9732132f30
Jeremstym <jeremstym@gmail.com>
2023-06-08 15:04:42 +0100

    Fix print draft

--- draft.py
+++ draft.py
@@ -8,13 +8,15 @@ import os
 import torch
 
 if __name__ == '__main__':
-    torch.cuda.device_count()
-    torch.cuda.get_device_name(0)
-    torch.cuda.is_available()
-    torch.cuda.current_device()
-    torch.cuda.device(0)
-    torch.cuda.empty_cache()
-    torch.cuda.memory_allocated()
-    torch.cuda.memory_cached()
-    torch.cuda.max_memory_allocated()
-    torch.cuda.max_memory_cached()
+    print(
+        torch.cuda.device_count(),
+        torch.cuda.get_device_name(0),
+        torch.cuda.is_available(),
+        torch.cuda.current_device(),
+        torch.cuda.device(0),
+        torch.cuda.empty_cache(),
+        torch.cuda.memory_allocated(),
+        torch.cuda.memory_cached(),
+        torch.cuda.max_memory_allocated(),
+        torch.cuda.max_memory_cached()
+    )
