7f9f505c1342d0d30676d95c0bca0369c66bd26f
Jeremstym <jeremstym@gmail.com>
2023-06-09 14:30:47 +0100

    Clean file

--- data/dataset_stnet.py
+++ data/dataset_stnet.py
@@ -17,7 +17,8 @@ import torch
 from torch import nn
 from torchvision import transforms
 import torch.utils.data as data
-import models.inception_STnet as inception_STnet
+import torchvision
+from torchvision.models.inception import Inception_V3_Weights
 
 import pickle as pkl
 from PIL import Image
@@ -25,9 +26,25 @@ from glob import glob
 import re
 from tqdm import tqdm
 
-model = inception_STnet.model
-model.eval()
+### ---------------- Customized Inception model ------------------------
+
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
+model = torchvision.models.inception_v3(weights=Inception_V3_Weights.DEFAULT)
+
+
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
+
+    def forward(self, x):
+        return x
+
+
+model.fc = Identity()
+
+model.eval()
+model.to(device)
+model.eval()
 
 ### ---------------- Pre-processing for images ------------------
 
@@ -111,6 +128,7 @@ def concat_tsv(path: str, bestgene: list) -> pd.DataFrame:
         pbar.set_description(f"Processing {tissue_name}")
     return df
 
+
 #     df = pd.DataFrame()
 #     for sub_path in tqdm(glob(path + "/*/", recursive=True)):
 #         pbar = tqdm(glob(sub_path + "/*.tsv", recursive=True))
@@ -131,33 +149,29 @@ def concat_tsv(path: str, bestgene: list) -> pd.DataFrame:
 #     return df
 
 
-if __name__ == "__main__":
-    path = "/import/pr_minos/jeremie/data"
-    with open(path + "/std_genes_avg.pkl", "rb") as f:
-        bestgene = list(pkl.load(f).index[:900])
-    df = concat_tsv(path, bestgene)
-    with open(path + "/tsv_concatened.pkl", "wb") as f:
-        pkl.dump(df, f)
+# if __name__ == "__main__":
+#     path = "/import/pr_minos/jeremie/data"
+#     with open(path + "/std_genes_avg.pkl", "rb") as f:
+#         bestgene = list(pkl.load(f).index[:900])
+#     df = concat_tsv(path, bestgene)
+#     with open(path + "/tsv_concatened.pkl", "wb") as f:
+#         pkl.dump(df, f)
 
 ### ---------------- Create dataset ------------------
 
 
 class Phenotypes(data.Dataset):
-    def __init__(self, path: str, model=model, device=device) -> None:
+    def __init__(
+        self, tsv_concatened, embeddings_dict, model=model, device=device
+    ) -> None:
         super().__init__()
-        self.path = path
+        self.genotypes = tsv_concatened
+        self.embeddings_dict = embeddings_dict
         self.model = model
         self.device = device
         self.selection_tensor = torch.tensor(
             [[552, 1382, 1171, 699, 663, 1502, 588, 436, 1222, 617]]
         ).to(self.device)
-
-        with open(path + "/embeddings_dict.pkl", "rb") as f:
-            self.embeddings_dict = pkl.load(f)
-        with open(path + "/tsv_concatened.pkl", "rb") as f:
-            self.genotypes = pkl.load(f)
-
-        # self.genotypes = self.concat_tsv()
 
     def __len__(self):
         return len(self.embeddings_dict)
@@ -167,31 +181,6 @@ class Phenotypes(data.Dataset):
             torch.tensor(self.genotypes.iloc[index].values),
             self.embeddings_dict[index][self.selection_tensor],
         )
-
-    # def tsv_processing(self, tissue_name: str, df: pd.DataFrame) -> pd.DataFrame:
-    #     mask = list(df.filter(regex="ambiguous"))
-    #     filtered_df = df[df.columns.drop(mask)]
-    #     filtered_df = filtered_df.rename(columns={"Unnamed: 0": "id"})
-    #     filtered_df["id"] = filtered_df["id"].apply(lambda x: tissue_name + "_" + x)
-    #     filtered_df = filtered_df.set_index("id")
-
-    #     return filtered_df[self.bestgene]
-
-    # def concat_tsv(self):
-    #     df = pd.DataFrame()
-    #     pbar = tqdm(glob(self.path + "/*/*[0-9].tsv", recursive=True))
-    #     for path_tsv in pbar:
-    #         m = re.search("data/(.*)/(.*).tsv", path_tsv)
-    #         if m:
-    #             tissue_name = m.group(2)
-    #             with open(path_tsv, "rb") as f:
-    #                 df_tsv = pd.read_csv(f, sep="\t")
-    #             df_tsv = self.tsv_processing(tissue_name, df_tsv)
-    #             df = pd.concat([df, df_tsv])
-    #         else:
-    #             raise ValueError("Path not found")
-    #         pbar.set_description(f"Processing {tissue_name}")
-    #     return df
 
 
 ### ---------------- Brouillon ------------------
