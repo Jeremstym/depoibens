5ff386cd597971184c7832dcff976b03f2e693dc
Jeremstym <jeremstym@gmail.com>
2023-06-09 14:47:37 +0100

    Change index for phenotypes

--- data/dataset_stnet.py
+++ data/dataset_stnet.py
@@ -176,7 +176,8 @@ class Phenotypes(data.Dataset):
     def __len__(self):
         return len(self.embeddings_dict)
 
-    def __getitem__(self, index):
+    def __getitem__(self, idx_number: int):
+        index = list(self.embeddings_dict.keys())[idx_number]
         return (
             torch.tensor(self.genotypes.iloc[index].values),
             self.embeddings_dict[index][self.selection_tensor],
