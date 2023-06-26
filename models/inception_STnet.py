d5b5d4ab686384f4af03f10ef065f2996baae2b0
Jeremstym <jeremstym@gmail.com>
2023-06-26 14:52:23 +0100

    Load best model for test instead final model

--- models/inception_STnet.py
+++ models/inception_STnet.py
@@ -339,7 +339,7 @@ def main(
             batch_norm=args["batch_norm"],
         )
         model.load_state_dict(
-            torch.load("/projects/minos/jeremie/data/outputs/final_model.pth")[
+            torch.load("/projects/minos/jeremie/data/outputs/best_model.pth")[
                 "model_state_dict"
             ]
         )
