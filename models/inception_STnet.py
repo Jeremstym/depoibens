6c7cc4abcc2d31122bc61372cfcbf62403b46047
Jeremstym <jeremstym@gmail.com>
2023-06-27 18:25:35 +0100

    Save best model 2 without batch norm

--- models/inception_STnet.py
+++ models/inception_STnet.py
@@ -350,7 +350,7 @@ def main(
             batch_norm=args["batch_norm"],
         )
         model.load_state_dict(
-            torch.load("/projects/minos/jeremie/data/outputs/best_model2.pth")[
+            torch.load("/projects/minos/jeremie/data/outputs/best_model2_nobn.pth")[
                 "model_state_dict"
             ]
         )
