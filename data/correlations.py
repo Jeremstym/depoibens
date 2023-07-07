c0ecec62a50d3d8ec80b2958519901efb36fa500
Jeremstym <jeremstym@gmail.com>
2023-07-07 17:23:42 +0100

    Load state dict model

--- data/correlations.py
+++ data/correlations.py
@@ -16,8 +16,24 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-import torch 
+import torch
 
-model = torch.load("/projects/minos/jeremie/data/outputs/best_model_dino.pth")
+from models.inception_STnet import Regression_STnet
+
+INPUT_SIZE = 900
+OUTPUT_SIZE = 768
+HIDDEN_SIZE = 1536
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = Regression_STnet(
+    input_size=INPUT_SIZE, hidden_size=HIDDEN_SIZE, output_size=OUTPUT_SIZE
+)
+
+model.load_state_dict(
+    torch.load(
+        "/projects/minos/jeremie/data/outputs/best_model_dino.pth", map_location=torch.device(device)
+    )["model_state_dict"]
+)
 model.eval()
+model.to(device)
 print(model)
