787fd5fb5094c234f4ff80a722a23e7083ca976b
Jeremstym <jeremstym@gmail.com>
2023-07-24 14:28:19 +0100

    Format

--- models/vanillaGAN.py
+++ models/vanillaGAN.py
@@ -29,9 +29,6 @@ ngf = 64  # size of feature maps in generator
 ndf = 64  # size of feature maps in discriminator
 
 
-
-
-
 ### -------------- Image processing ------------------
 preprocess = transforms.Compose(
     [
@@ -174,7 +171,6 @@ def weights_init(m):
     elif classname.find("BatchNorm") != -1:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
-
 
 
 ### -------------- Brouillon -----------------------
