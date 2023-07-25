336f448d269b144f7702dfd1e036878d541d0057
Jeremstym <jeremstym@gmail.com>
2023-07-25 15:59:58 +0100

    Cropsize to 300

--- models/train_vanillaGAN.py
+++ models/train_vanillaGAN.py
@@ -65,8 +65,8 @@ def main():
 
     preprocess = transforms.Compose(
         [
-            transforms.Resize(299),
-            transforms.CenterCrop(299),
+            transforms.Resize(300),
+            transforms.CenterCrop(300),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
