9028db01711f2113ca280564586e23f4918d903f
Jeremstym <jeremstym@gmail.com>
2023-07-25 15:59:42 +0100

    Comment lines

--- models/vanillaGAN.py
+++ models/vanillaGAN.py
@@ -29,15 +29,6 @@ ngf = 64  # size of feature maps in generator
 ndf = 64  # size of feature maps in discriminator
 
 
-### -------------- Image processing ------------------
-preprocess = transforms.Compose(
-    [
-        transforms.Resize(299),
-        transforms.CenterCrop(299),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ]
-)
 
 ### ----------- Networks ----------------------------
 
@@ -213,3 +204,12 @@ def weights_init(m):
 
 #     def forward(self, input):
 #         return self.main(input)
+
+# preprocess = transforms.Compose(
+#     [
+#         transforms.Resize(299),
+#         transforms.CenterCrop(299),
+#         transforms.ToTensor(),
+#         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+#     ]
+# )
