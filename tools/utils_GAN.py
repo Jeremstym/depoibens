e7361e9dbf354818f2baeec9ba516765a944a0b1
Jeremstym <jeremstym@gmail.com>
2023-07-27 16:55:41 +0100

    Fix methods issues with show tensor images

--- tools/utils_GAN.py
+++ tools/utils_GAN.py
@@ -111,6 +111,8 @@ def show_tensor_images(
     image_unflat = image_tensor.detach().cpu().view(-1, *size)
     image_grid = (
         make_grid(image_unflat[:num_images], nrow=5)
+        .mul(0.5)
+        .add(0.5)
         .mul(255)
         .permute(1, 2, 0)
         .to("cpu", torch.uint8)
@@ -152,8 +154,6 @@ def show_final_grid(
     image_unflat = img_list[-1].detach().cpu().view(-1, *size)
     image_grid = (
         make_grid(image_unflat[:num_image], nrow=5)
-        .mul(0.5)
-        .add_(0.5)
         .mul(255)
         .permute(1, 2, 0)
         .to("cpu", torch.uint8)
