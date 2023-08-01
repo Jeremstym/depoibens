103aa8bc9d7229a0065ca9ba53f441e6e6391b97
Jeremstym <jeremstym@gmail.com>
2023-08-01 17:20:40 +0100

    Add 2 gpus

--- models/train_ligthningGAN.py
+++ models/train_ligthningGAN.py
@@ -62,8 +62,8 @@ def main():
     ]
     trainer = pl.Trainer(
         accelerator="gpu",
-        # strategy="ddp",
-        devices=[0],
+        strategy="ddp",
+        devices=[0, 1],
         max_epochs=EPOCHS,
         logger=loggers,
         log_every_n_steps=1
