4f74f68a0ac20bbf0e55b3fe440d677084078d72
Jeremstym <jeremstym@gmail.com>
2023-08-03 15:26:00 +0100

    Un-use custom op for conv2d

--- stylegan2-repo/torch_utils/ops/conv2d_gradfix.py
+++ stylegan2-repo/torch_utils/ops/conv2d_gradfix.py
@@ -50,9 +50,9 @@ def _should_use_custom_op(input):
         return False
     if input.device.type != 'cuda':
         return False
-    if any(torch.__version__.startswith(x) for x in ['1.7.', '1.8.', '1.9', '2.0']):
+    if any(torch.__version__.startswith(x) for x in ['1.7.', '1.8.', '1.9']): # latter add '2.0' to use custom op
         return True
-    warnings.warn(f'conv2d_gradfix not supported on PyTorch {torch.__version__}. Falling back to torch.nn.functional.conv2d().')
+    # warnings.warn(f'conv2d_gradfix not supported on PyTorch {torch.__version__}. Falling back to torch.nn.functional.conv2d().')
     return False
 
 def _tuple_of_ints(xs, ndim):
