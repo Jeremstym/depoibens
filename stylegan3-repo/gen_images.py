a4f231e3a664484166ddd0163af077e965a04412
Jeremstym <jeremstym@gmail.com>
2023-09-25 16:02:35 +0100

    Set real img to device

--- stylegan3-repo/gen_images.py
+++ stylegan3-repo/gen_images.py
@@ -147,7 +147,7 @@ def generate_images(
         img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
         img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
         # real_img = (real_image.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
-        real_img = torch.from_numpy(real_image)
+        real_img = torch.from_numpy(real_image).to(device)
         combined_img = torch.cat((real_img, img), dim=2)
         PIL.Image.fromarray(combined_img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed{seed:04d}.png')
 
