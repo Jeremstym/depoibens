d98a75c5011def802cbfd389f48b8eeb9bfdfe07
Jeremstym <jeremstym@gmail.com>
2023-09-26 10:41:03 +0100

    Unpack unused canal

--- stylegan3-repo/gen_images.py
+++ stylegan3-repo/gen_images.py
@@ -142,7 +142,7 @@ def generate_images(
     if genes is True:
         # grid = np.empty((1, 256, 256 * len(seeds), 3))
         print(list_of_images[0][0].shape)
-        w, h = list_of_images[0][0].shape
+        _c, w, h = list_of_images[0][0].shape
         gw, gh = len(seeds), len(class_idx)
         canvas = PIL.Image.new('RGB', (h * gh, w * gw), 'white')
         list_of_PIL_images = []            
