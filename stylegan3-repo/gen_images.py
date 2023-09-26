f16d424a77ebc2c16dbd625ee7010c3c1dd15854
Jeremstym <jeremstym@gmail.com>
2023-09-26 10:38:19 +0100

    Unpack shape image

--- stylegan3-repo/gen_images.py
+++ stylegan3-repo/gen_images.py
@@ -141,7 +141,7 @@ def generate_images(
 
     if genes is True:
         # grid = np.empty((1, 256, 256 * len(seeds), 3))
-        w, h = list_of_images[0][0].size
+        w, h = list_of_images[0][0].shape
         gw, gh = len(seeds), len(class_idx)
         canvas = PIL.Image.new('RGB', (h * gh, w * gw), 'white')
         list_of_PIL_images = []            
