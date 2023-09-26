dfe143b153f1acc0638699cbc2867dcfd565f510
Jeremstym <jeremstym@gmail.com>
2023-09-26 16:15:20 +0100

    Create dataloader builder

--- evaluation/tumo_dataset.py
+++ evaluation/tumo_dataset.py
@@ -80,3 +80,33 @@ class TumoDataset(data.Dataset):
     def __getitem__(self, idx_number: int):
         index = list(self.dict.keys())[idx_number]
         return self.dict[index], self.tumor[index]
+
+
+def create_dataloader(
+    tumor_path: str,
+    path_to_image: str,
+    batch_size: int = BATCH_SIZE,
+    valid_split: float = VALID_SPLIT,
+    num_workers: int = NUM_WORKERS,
+    shuffle: bool = True,
+):
+    dataset = TumoDataset(tumor_path, path_to_image)
+    # Creating data indices for training and validation splits:
+    dataset_size = len(dataset)
+    indices = list(range(dataset_size))
+    split = int(np.floor(valid_split * dataset_size))
+    if shuffle:
+        np.random.shuffle(indices)
+    train_indices, valid_indices = indices[split:], indices[:split]
+
+    # Creating PT data samplers and loaders:
+    train_sampler = data.SubsetRandomSampler(train_indices)
+    valid_sampler = data.SubsetRandomSampler(valid_indices)
+
+    train_loader = data.DataLoader(
+        dataset, batch_size=batch_size, sampler=train_sampler, num_workers=num_workers
+    )
+    valid_loader = data.DataLoader(
+        dataset, batch_size=batch_size, sampler=valid_sampler, num_workers=num_workers
+    )
+    return train_loader, valid_loader
