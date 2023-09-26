a406a48cabab068a19fd73f40b0eea2afc85c570
Jeremstym <jeremstym@gmail.com>
2023-09-26 09:59:33 +0100

    Adapt generator to make grid

--- stylegan3-repo/gen_images.py
+++ stylegan3-repo/gen_images.py
@@ -74,7 +74,7 @@ def make_transform(translate: Tuple[float,float], angle: float):
 @click.option('--network', 'network_pkl', help='Network pickle filename', required=True)
 @click.option('--seeds', type=parse_range, help='List of random seeds (e.g., \'0,1,4-6\')', required=True)
 @click.option('--trunc', 'truncation_psi', type=float, help='Truncation psi', default=1, show_default=True)
-@click.option('--class', 'class_idx', type=int, help='Class label (unconditional if not specified)')
+@click.option('--class', 'class_idx', type=parse_range, help='Class label (optional list, unconditional if not specified, e.g., \'0,1,4-6\')')
 @click.option('--noise-mode', help='Noise mode', type=click.Choice(['const', 'random', 'none']), default='const', show_default=True)
 @click.option('--translate', help='Translate XY-coordinate (e.g. \'0.3,1\')', type=parse_vec2, default='0,0', show_default=True, metavar='VEC2')
 @click.option('--rotate', help='Rotation angle in degrees', type=float, default=0, show_default=True, metavar='ANGLE')
@@ -89,7 +89,7 @@ def generate_images(
     outdir: str,
     translate: Tuple[float,float],
     rotate: float,
-    class_idx: Optional[int],
+    class_idx: Optional[List[int]],
     genes: bool,
     data: str
 ):
@@ -118,50 +118,112 @@ def generate_images(
     # Labels.
     label = torch.zeros([1, G.c_dim], device=device)
     if G.c_dim != 0:
-        if class_idx is None and genes is False:
+        if class_idx is None:
             raise click.ClickException('Must specify class label with --class when using a conditional network')
-        if genes is True:
+        if genes is True and len(class_idx) == 1:
             training_set = import_dataset(genes=genes, data=data, gene_size=G.c_dim)
-            label = training_set.get_label(class_idx)
+            label = training_set.get_label(class_idx[0])
             label = torch.from_numpy(label).unsqueeze(0).to(device)
-            real_image = training_set[class_idx][0]
+            real_image = training_set[class_idx[0]][0]
+        elif genes is True and len(class_idx) > 1:
+            training_set = import_dataset(genes=genes, data=data, gene_size=G.c_dim)
+            list_of_images = []
+            for idx in class_idx:
+                label = training_set.get_label(idx)
+                label = torch.from_numpy(label).unsqueeze(0).to(device)
+                real_image = training_set[idx][0]
+                list_of_images.append([real_image, label])
         else:
             label[:, class_idx] = 1
     else:
         if class_idx is not None:
             print ('warn: --class=lbl ignored when running on an unconditional network')
 
-    # Generate images.
-    for seed_idx, seed in enumerate(seeds):
-        print('Generating image for seed %d (%d/%d) ...' % (seed, seed_idx, len(seeds)))
-        z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
+    if genes is True and len(class_idx) > 1:
+        grid = np.empty((1, 256, 256 * len(class_idx), 3))
+        for real_image, label in list_of_images:            
+            real_img = np.expand_dims(real_image.transpose(1, 2, 0), axis=0)
+            combined_img = real_image
+            # Generate images.
+            for seed_idx, seed in enumerate(seeds):
+                print('Generating image for seed %d (%d/%d) ...' % (seed, seed_idx, len(seeds)))
+                z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
 
-        # Construct an inverse rotation/translation matrix and pass to the generator.  The
-        # generator expects this matrix as an inverse to avoid potentially failing numerical
-        # operations in the network.
-        if hasattr(G.synthesis, 'input'):
-            m = make_transform(translate, rotate)
-            m = np.linalg.inv(m)
-            G.synthesis.input.transform.copy_(torch.from_numpy(m))
+                # Construct an inverse rotation/translation matrix and pass to the generator.  The
+                # generator expects this matrix as an inverse to avoid potentially failing numerical
+                # operations in the network.
+                if hasattr(G.synthesis, 'input'):
+                    m = make_transform(translate, rotate)
+                    m = np.linalg.inv(m)
+                    G.synthesis.input.transform.copy_(torch.from_numpy(m))
 
-        img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
-        img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+                if len(seeds) > 1 and seed_idx != len(seeds) - 1:
+                    gen_img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
+                    gen_img = (gen_img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+                    gen_img = gen_img.cpu().numpy()
+                    combined_img = np.concatenate((combined_img, gen_img), axis=2)
+                    continue
+                elif len(seeds) == 1:
+                    img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
+                    img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+                    combined_img = np.concatenate((real_img, img.cpu().numpy()), axis=2)
+            
+            grid = np.concatenate((grid, combined_img), axis=1)
+
+        grid = grid[:, 256:, :, :]
+        # PIL.Image.fromarray(grid[0], 'RGB').save(f'{outdir}/seed{seed:04d}.png')
+        PIL.Image.fromarray(grid[0], 'RGB').save(f'{outdir}_grid.png')
+
+    else:
         real_img = np.expand_dims(real_image.transpose(1, 2, 0), axis=0)
-        # real_img = (real_image.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
-        # real_img = torch.from_numpy(real_image).unsqueeze(0).to(device)
-        # _N, C, H, W = real_image.shape
-        combined_img = np.concatenate((real_img, img.cpu().numpy()), axis=2)
-        PIL.Image.fromarray(combined_img[0], 'RGB').save(f'{outdir}/seed{seed:04d}.png')
+        combined_img = real_image
+        # Generate images.
+        for seed_idx, seed in enumerate(seeds):
+            print('Generating image for seed %d (%d/%d) ...' % (seed, seed_idx, len(seeds)))
+            z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
+
+            # Construct an inverse rotation/translation matrix and pass to the generator.  The
+            # generator expects this matrix as an inverse to avoid potentially failing numerical
+            # operations in the network.
+            if hasattr(G.synthesis, 'input'):
+                m = make_transform(translate, rotate)
+                m = np.linalg.inv(m)
+                G.synthesis.input.transform.copy_(torch.from_numpy(m))
+
+            if len(seeds) > 1 and seed_idx != len(seeds) - 1:
+                gen_img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
+                gen_img = (gen_img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+                gen_img = gen_img.cpu().numpy()
+                combined_img = np.concatenate((combined_img, gen_img), axis=2)
+                continue
+            elif len(seeds) == 1:
+                img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
+                img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+                combined_img = np.concatenate((real_img, img.cpu().numpy()), axis=2)
+
+        # PIL.Image.fromarray(combined_img[0], 'RGB').save(f'{outdir}/seed{seed:04d}.png')
+        PIL.Image.fromarray(combined_img[0], 'RGB').save(f'{outdir}_grid.png')
 
 
 def import_dataset(genes: bool, data:str, gene_size: int):
     # Training set.
-    training_set_kwargs, dataset_name = init_dataset_kwargs(data=data, is_pickle=genes)
+    training_set_kwargs, _dataset_name = init_dataset_kwargs(data=data, is_pickle=genes)
     training_set_kwargs.use_labels = True
     training_set_kwargs.xflip = False
     training_set_kwargs.gene_size = gene_size
     training_set = dnnlib.util.construct_class_by_name(**training_set_kwargs) # subclass of training.dataset.Dataset
     return training_set
+
+def make_grid(imgs: List[PIL.Image.Image], grid_size: Tuple[int, int]) -> PIL.Image.Image:
+    '''Make a grid of images.'''
+    w, h = imgs[0].size
+    gw, gh = grid_size
+    canvas = PIL.Image.new('RGB', (w * gw, h * gh), 'white')
+    for idx, img in enumerate(imgs):
+        x = idx % gw
+        y = idx // gw
+        canvas.paste(img, (x * w, y * h))
+    return canvas
 
 
 #----------------------------------------------------------------------------
